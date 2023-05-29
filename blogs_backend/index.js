e25b301f05f33680c48f194daec38b997a1dfefe
MangJuan08 <72972235+MangJuan08@users.noreply.github.com>
2023-05-29 14:07:26 +0200

    getDatiUtente

--- blogs_backend/index.js
+++ blogs_backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3001;
@@ -15,9 +16,9 @@ const con = mysql.createConnection({
 });
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.post("/login", (req, res) => {
   let bodyReq = [];
@@ -30,81 +31,84 @@ app.post("/login", (req, res) => {
 
   con.connect((err) => {
     con.query(
-      "SELECT * FROM users where username='" + loginDetails.username + "'" + "&& password='"+loginDetails.password+"'",
+      "SELECT * FROM users where username='" +
+        loginDetails.username +
+        "'" +
+        "&& password='" +
+        loginDetails.password +
+        "'",
       (error, result, fields) => {
         if (result.length > 0) {
           let results = Object.values(JSON.parse(JSON.stringify(result)));
-         let id = results[0].idUser;
-          const token = jwt.sign({id}, "secretKey", { expiresIn: "60m" });
+          let id = results[0].idUser;
+          const token = jwt.sign({ id }, "secretKey", { expiresIn: "60m" });
 
           res.json({
-            login:true,
+            login: true,
             isAdmin: results[0].userType === "admin" ? true : false,
             results,
             token,
-            id:id
+            id: id,
           });
-
         } else {
-          res.json({message:"login failed"});
+          res.json({ message: "login failed" });
         }
       }
     );
   });
 });
 
-
 app.get("/getPosts", (req, res) => {
-
   con.connect((err) => {
-    con.query(
-      "SELECT * FROM posts",
-      (error, result, fields) => {
-       res.send(result)
-      }
-    );
+    con.query("SELECT * FROM posts", (error, result, fields) => {
+      res.send(result);
+    });
   });
 });
 
-
 app.get("/getSinglePost/:id", (req, res) => {
-
-
-console.log(req.params.id)
+  console.log(req.params.id);
 
   con.connect((err) => {
     con.query(
       "SELECT * FROM posts where idpost='" + req.params.id + "'",
       (error, result, fields) => {
-       res.send(result)
+        res.send(result);
       }
     );
   });
 });
 
+app.get("/datiUtente/:idUtente", (req, res, err) => {
+  con.connect((err) => {
+    con.query(
+      "SELECT * FROM users where idUser='" + req.params.idUtente + "'",
+      (error, result, fields) => {
+        res.send(result);
+      }
+    );
+  });
+});
 
 const verifyJWT = (req, res, next) => {
-  
   const token = req.headers["access-token"];
-  if(!token) {
+  if (!token) {
     return res.json("need token");
-  }
-  else {
-    jwt.verify(token, "secretKey", (err,decoded) => {
-      if(err) {
-        res.json("not authenticated")
+  } else {
+    jwt.verify(token, "secretKey", (err, decoded) => {
+      if (err) {
+        res.json("not authenticated");
       } else {
         req.id = decoded.id;
         next();
       }
-    })
+    });
   }
-   
-}
+};
 
 app.get("/controlAuth", verifyJWT, (req, res) => {
-  return res.json("authenticated")
-  });
+  return res.json("authenticated");
+});
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
--- blogs_frontend/src/pages/Profile.js
+++ blogs_frontend/src/pages/Profile.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import { NavbarSection } from '../components/NavbarSection'
+import React, { useEffect, useState } from "react";
+import { NavbarSection } from "../components/NavbarSection";
+import axios from "axios";
 
 const Profile = () => {
+
+  const [dati, setDati] = useState([]);
+
+
+
+  const getDatiUtente = () => {
+    const idUtente = localStorage.getItem("idUtente");
+
+    axios
+      .get(`http://localhost:3001/datiUtente/${idUtente}`)
+      .then((res) => {
+
+        console.log(res);
+        setDati(res)
+        console.log(res.data[0].username)
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getDatiUtente();
+  }, []);
+
+
+
   return (
     <div>
-        <NavbarSection/><br></br>
-        <div className="container">Profile</div>
+      <NavbarSection />
+      <br></br>
+      <div className="container">Profile</div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
